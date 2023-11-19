6549081a45b64d203e10cc3fff83f948b6005e04
hasan-graphy <mohd.ali@graphy.com>
2023-11-20 00:42:54 +0530

    landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
+import { ThemeProvider } from '@/components/providers/theme-provider'
+import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Motion',
-  description: 'Motion is the connected workspace where better, faster work happens.',
+  title: 'Jotion',
+  description: 'The connected workspace where better, faster work happens.',
   icons: {
     icon: [
       {
@@ -29,8 +30,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          storageKey="jotion-theme-2"
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
