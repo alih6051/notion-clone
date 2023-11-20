64977ab8f36e56d2dd0f5512fe18af9dc30d1748
hasan-graphy <mohd.ali@graphy.com>
2023-11-21 00:30:54 +0530

    convex and clerk setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
-import { ThemeProvider } from '@/components/providers/theme-provider'
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'NotionLite',
-  description: 'The connected workspace where better, faster work happens.',
+  title: "NotionLite",
+  description: "The connected workspace where better, faster work happens.",
   icons: {
     icon: [
       {
@@ -19,29 +20,31 @@ export const metadata: Metadata = {
         media: "(prefers-color-scheme: dark)",
         url: "/logo-dark.svg",
         href: "/logo-dark.svg",
-      }
-    ]
-  }
-}
+      },
+    ],
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-          storageKey="notion-lite-theme-2"
-        >
-          {children}
-        </ThemeProvider>
+        <ConvexClientProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="notion-lite-theme-2"
+          >
+            {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
       </body>
     </html>
-  )
+  );
 }
---
+++ convex/auth.config.js
@@ -0,0 +1,10 @@
+export default {
+    providers: [
+      {
+        domain: "https://composed-antelope-79.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+  
+  
