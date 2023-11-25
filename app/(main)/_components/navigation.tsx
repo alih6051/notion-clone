e103a5d7d158196a6c55148d9aefe71d1598e2dd
Hasan Ali <mohd.ali@graphy.com>
2023-11-25 23:35:01 +0530

    document list component

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import { 
-  ChevronsLeft, 
-  MenuIcon, 
-  PlusCircle, 
-  Search, 
+import {
+  ChevronsLeft,
+  MenuIcon,
+  PlusCircle,
+  Search,
   Settings
 } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
-import { useQuery, useMutation } from "convex/react";
+import { useMutation } from "convex/react";
 import { toast } from "sonner";
 
 import { cn } from "@/lib/utils";
@@ -18,11 +18,11 @@ import { api } from "@/convex/_generated/api";
 
 import { UserItem } from "./user-item";
 import { Item } from "./item";
+import { DocumentList } from "./document-list";
 
 export const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
-  const documents = useQuery(api.documents.get);
   const create = useMutation(api.documents.create);
 
   const isResizingRef = useRef(false);
@@ -156,11 +156,7 @@ export const Navigation = () => {
           />
         </div>
         <div className="mt-4">
-          {documents?.map((document) => (
-            <p key={document._id}>
-              {document.title}
-            </p>
-          ))}
+          <DocumentList />
         </div>
         <div
           onMouseDown={handleMouseDown}
@@ -174,7 +170,7 @@ export const Navigation = () => {
           "absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]",
           isResetting && "transition-all ease-in-out duration-300",
           isMobile && "left-0 w-full",
-          !isCollapsed && "hidden"
+          !isCollapsed && 'hidden'
         )}
       >
         <nav className="bg-transparent px-3 py-2 w-full">
