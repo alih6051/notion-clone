bd6257f60bef4ba1634ef2d1778a50e3816abbfe
Hasan Ali <mohd.ali@graphy.com>
2023-11-27 23:42:31 +0530

    added search

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,8 +2,11 @@
 
 import { useConvexAuth } from "convex/react";
 import { redirect } from "next/navigation";
-import { Navigation } from "./_components/navigation";
+
 import { Spinner } from "@/components/spinner";
+import { SearchCommand } from "@/components/search-command";
+
+import { Navigation } from "./_components/navigation";
 
 const MainLayout = ({
   children
@@ -28,6 +31,7 @@ const MainLayout = ({
     <div className="h-full flex dark:bg-[#1F1F1F]">
       <Navigation />
       <main className="flex-1 h-full overflow-y-auto">
+        <SearchCommand />
         {children}
       </main>
     </div>
