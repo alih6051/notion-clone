a1d67b4f6c507c5ef1a24553cb1ebc8089e51d1c
Hasan Ali <mohd.ali@graphy.com>
2023-11-28 00:16:48 +0530

    padding fix

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -197,7 +197,8 @@ export const Navigation = () => {
         className={cn(
           "absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]",
           isResetting && "transition-all ease-in-out duration-300",
-          isMobile && "left-0 w-full"
+          isMobile && "left-0 w-full",
+          !isCollapsed && "hidden"
         )}
       >
         <nav className="bg-transparent px-3 py-2 w-full">
