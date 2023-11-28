b8bc4045debaaa964b9f1a1d9ddbf9e5367ab17a
Hasan Ali <mohd.ali@graphy.com>
2023-11-29 00:27:23 +0530

    fix: banner responsiveness

--- app/(main)/_components/banner.tsx
+++ app/(main)/_components/banner.tsx
@@ -11,11 +11,9 @@ import { ConfirmModal } from "@/components/modals/confirm-modal";
 
 interface BannerProps {
   documentId: Id<"documents">;
-};
+}
 
-export const Banner = ({
-  documentId
-}: BannerProps) => {
+export const Banner = ({ documentId }: BannerProps) => {
   const router = useRouter();
 
   const remove = useMutation(api.documents.remove);
@@ -27,7 +25,7 @@ export const Banner = ({
     toast.promise(promise, {
       loading: "Deleting note...",
       success: "Note deleted!",
-      error: "Failed to delete note."
+      error: "Failed to delete note.",
     });
 
     router.push("/documents");
@@ -39,32 +37,32 @@ export const Banner = ({
     toast.promise(promise, {
       loading: "Restoring note...",
       success: "Note restored!",
-      error: "Failed to restore note."
+      error: "Failed to restore note.",
     });
   };
 
   return (
-    <div className="w-full bg-rose-500 text-center text-sm p-2 text-white flex items-center gap-x-2 justify-center">
-      <p>
-        This page is in the Trash.
-      </p>
-      <Button
-        size="sm"
-        onClick={onRestore}
-        variant="outline"
-        className="border-white bg-transparent hover:bg-primary/5 text-white hover:text-white p-1 px-2 h-auto font-normal"
-      >
-        Restore page
-      </Button>
-      <ConfirmModal onConfirm={onRemove}>
+    <div className="w-full bg-rose-500 text-center text-sm p-2 text-white flex items-center gap-x-2 gap-y-2 justify-center flex-col  md:flex-row">
+      <p>This page is in the Trash.</p>
+      <div className="flex items-center gap-x-2">
         <Button
           size="sm"
+          onClick={onRestore}
           variant="outline"
           className="border-white bg-transparent hover:bg-primary/5 text-white hover:text-white p-1 px-2 h-auto font-normal"
         >
-          Delete forever
+          Restore page
         </Button>
-      </ConfirmModal>
+        <ConfirmModal onConfirm={onRemove}>
+          <Button
+            size="sm"
+            variant="outline"
+            className="border-white bg-transparent hover:bg-primary/5 text-white hover:text-white p-1 px-2 h-auto font-normal"
+          >
+            Delete forever
+          </Button>
+        </ConfirmModal>
+      </div>
     </div>
-  )
-}
+  );
+};
