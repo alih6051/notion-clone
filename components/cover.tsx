5fb922a454258268e0bfd66ca9ada0066ead9ef6
Hasan Ali <mohd.ali@graphy.com>
2023-12-03 03:27:23 +0530

    editor added

--- components/cover.tsx
+++ components/cover.tsx
@@ -5,11 +5,13 @@ import { ImageIcon, X } from "lucide-react";
 import { useMutation } from "convex/react";
 import { useParams } from "next/navigation";
 
+import { Skeleton } from "@/components/ui/skeleton";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { useCoverImage } from "@/hooks/use-cover-image";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
+import { useEdgeStore } from "@/lib/edgestore";
 
 interface CoverImageProps {
   url?: string;
@@ -20,11 +22,17 @@ export const Cover = ({
   url,
   preview,
 }: CoverImageProps) => {
+  const { edgestore } = useEdgeStore();
   const params = useParams();
   const coverImage = useCoverImage();
   const removeCoverImage = useMutation(api.documents.removeCoverImage);
 
-  const onRemove = () => {
+  const onRemove = async () => {
+    if (url) {
+      await edgestore.publicFiles.delete({
+        url: url
+      })
+    }
     removeCoverImage({
       id: params.documentId as Id<"documents">
     });
@@ -47,7 +55,7 @@ export const Cover = ({
       {url && !preview && (
         <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
           <Button
-            onClick={coverImage.onOpen}
+            onClick={() => coverImage.onReplace(url)}
             className="text-muted-foreground text-xs"
             variant="outline"
             size="sm"
@@ -67,5 +75,11 @@ export const Cover = ({
         </div>
       )}
     </div>
+  )
+}
+
+Cover.Skeleton = function CoverSkeleton() {
+  return (
+    <Skeleton className="w-full h-[12vh]" />
   )
 }
